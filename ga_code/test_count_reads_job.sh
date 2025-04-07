f4e4c9e44a3d00cecad28ba8e7ab54b74b314da6
Tobias-Giertz <tobias.giertz@gmail.com>
2025-04-07 16:23:20 +0200

    Update test_count_reads_job.sh

--- ga_code/test_count_reads_job.sh
+++ ga_code/test_count_reads_job.sh
@@ -4,15 +4,16 @@
 #SBATCH --reservation=uppmax2025-3-3_2
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 2
+#SBATCH -n 1
 #SBATCH -t 00:02:00
 #SBATCH -J test_count_reads
-#SBATCH --mail-type=ALL 
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=tobias.giertz.0318@student.uu.se
-#SBATCH --output=~/Genome_analysis/ga_code/ga_slurm_logs/test_count_reads-%j.out
+#SBATCH --output=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/test_count_reads-%j.out
+#SBATCH --error=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/test_count_reads-%j.err
 
-# Load modules
+# Load module
 module load bioinfo-tools
 
-# Run your script on one of the raw files
-~/Genome_analysis/ga_code/count_reads.sh ~/Genome_analysis/ga_code/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz
+# Run your count script
+/home/tobia/Genome_analysis/ga_code/count_reads.sh /home/tobia/Genome_analysis/ga_data/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz
