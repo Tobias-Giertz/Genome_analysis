e2454fe024798ec7f647ccda94934d874afd9982
Tobias-Giertz <tobias.giertz@gmail.com>
2025-04-07 15:22:15 +0200

    Create slurm_template.sh

---
+++ ga_code/slurm_template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -A uppmax2025-3-3     # Your project ID
+#SBATCH -p core               # Partition (core/short etc.)
+#SBATCH -n 2                  # Number of cores
+#SBATCH -t 01:00:00           # Time limit (hh:mm:ss)
+#SBATCH -J my_job_name        # Job name
+#SBATCH -o slurm-%j.out       # STDOUT (%j = job ID)
+#SBATCH -e slurm-%j.err       # STDERR
+
+# Load required modules
+module load bioinfo-tools
+module load samtools
+
+# Activate Conda environment if needed
+# source activate my_env
+
+# Your command goes here
+echo "Running my script..."
+./ga_code/count_reads.sh ga_data/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz
