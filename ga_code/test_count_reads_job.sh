e5efd0c3fcfdabe119fba28a9a6f7d23be5d9a08
Tobias-Giertz <tobias.giertz@gmail.com>
2025-04-07 15:34:01 +0200

    Create test_count_reads_job.sh

---
+++ ga_code/test_count_reads_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -A uppmax2025-3-3
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 00:02:00
+#SBATCH -J test_count_reads
+#SBATCH -o ga_code/ga_slurm_logs/test_count_reads-%j.out
+#SBATCH -e ga_code/ga_slurm_logs/test_count_reads-%j.err
+
+# Load necessary modules
+module load bioinfo-tools
+
+# Run your script on one of the raw files
+./ga_code/count_reads.sh ga_data/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz
