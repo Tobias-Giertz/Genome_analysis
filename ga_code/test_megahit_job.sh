1eeb993b950d34faca64a2f093ef179790ca7c8c
Tobias-Giertz <tobias.giertz@gmail.com>
2025-04-10 09:29:49 +0200

    Update test_megahit_job.sh

--- ga_code/test_megahit_job.sh
+++ ga_code/test_megahit_job.sh
@@ -16,8 +16,8 @@ module load bioinfo-tools
 module load megahit
 
 # Make output dir
-OUTDIR=/home/tobia/Genome_analysis/ga_analyses/02_assembly/test_megahit_out
-mkdir -p $OUTDIR
+OUTDIR=/home/tobia/Genome_analysis/ga_analyses/02_assembly/test_megahit_out_01
+
 
 # Run MEGAHIT with a subset of reads (e.g. first 100k lines = 25k reads)
 megahit \
