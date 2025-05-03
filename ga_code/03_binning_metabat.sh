5799787701ebf1957db89099702d57589e3b8f2c
Tobias-Giertz <tobias.giertz@gmail.com>
2025-05-03 12:24:02 +0200

    Update 03_binning_metabat.sh

--- ga_code/03_binning_metabat.sh
+++ ga_code/03_binning_metabat.sh
@@ -12,31 +12,37 @@
 #SBATCH --output=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/binning_metabat-%j.out
 #SBATCH --error=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/binning_metabat-%j.err
 
-# Load required modules
+# Load modules
 module load bioinfo-tools
 module load bwa
 module load samtools
-module load metabat
+module load singularity
 
-# Define paths
+# Paths
 CONTIGS=/home/tobia/Genome_analysis/ga_analyses/02_assembly/full_megahit_out/final.contigs.fa
-READ1=/home/tobia/Genome_analysis/ga_data/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz,/home/tobia/Genome_analysis/ga_data/raw_data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz
-READ2=/home/tobia/Genome_analysis/ga_data/raw_data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz,/home/tobia/Genome_analysis/ga_data/raw_data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz
+READ1_1=/home/tobia/Genome_analysis/ga_data/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz
+READ1_2=/home/tobia/Genome_analysis/ga_data/raw_data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz
+READ2_1=/home/tobia/Genome_analysis/ga_data/raw_data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz
+READ2_2=/home/tobia/Genome_analysis/ga_data/raw_data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz
 OUTDIR=/proj/uppmax2025-3-3/nobackup/tobia/03_binning
 mkdir -p $OUTDIR
 
-# Index the contigs
+# Index contigs
 bwa index $CONTIGS
 
-# Map reads to contigs and convert to BAM
-bwa mem -t 4 $CONTIGS $READ1 $READ2 | samtools view -Sb - > $OUTDIR/mapped.bam
+# Map both read pairs separately and merge BAMs
+bwa mem -t 4 $CONTIGS $READ1_1 $READ1_2 | samtools view -Sb - > $OUTDIR/tmp1.bam
+bwa mem -t 4 $CONTIGS $READ2_1 $READ2_2 | samtools view -Sb - > $OUTDIR/tmp2.bam
+
+samtools merge -@ 4 $OUTDIR/mapped.bam $OUTDIR/tmp1.bam $OUTDIR/tmp2.bam
+rm $OUTDIR/tmp1.bam $OUTDIR/tmp2.bam
 
 # Sort and index BAM
 samtools sort -@ 4 -o $OUTDIR/mapped.sorted.bam $OUTDIR/mapped.bam
 samtools index $OUTDIR/mapped.sorted.bam
 
-# Estimate depth per contig
-jgi_summarize_bam_contig_depths --outputDepth $OUTDIR/depth.txt $OUTDIR/mapped.sorted.bam
+# Summarize depth (inside container)
+singularity exec /tools/singularity/metabat2.sif jgi_summarize_bam_contig_depths --outputDepth $OUTDIR/depth.txt $OUTDIR/mapped.sorted.bam
 
-# Run MetaBAT to bin the contigs
-metabat -i $CONTIGS -a $OUTDIR/depth.txt -o $OUTDIR/bin
+# Run MetaBAT2 (inside container)
+singularity exec /tools/singularity/metabat2.sif metabat2 -i $CONTIGS -a $OUTDIR/depth.txt -o $OUTDIR/bin
