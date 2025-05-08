d82b6f043a8dbdb7eaaeb2a837500e3772fbafe5
Tobias-Giertz <tobias.giertz@gmail.com>
2025-05-08 20:17:35 +0200

    Update 04_2_bin_quality_checkm.sh

--- ga_code/04_2_bin_quality_checkm.sh
+++ ga_code/04_2_bin_quality_checkm.sh
@@ -7,19 +7,22 @@
 #SBATCH --mem=32G
 #SBATCH -t 04:00:00
 #SBATCH -J checkm_bins
-#SBATCH --mail-type=ALL
+#SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tobias.giertz.0318@student.uu.se
 #SBATCH --output=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/checkm-%j.out
 #SBATCH --error=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/checkm-%j.err
 
-# Load modules
+# Load CheckM
 module load bioinfo-tools
 module load CheckM/1.1.3
 
 # Define paths
 BINS_DIR=/proj/uppmax2025-3-3/nobackup/tobia/03_binning
-OUTDIR=/proj/uppmax2025-3-3/nobackup/tobia/04_2_checkm
-mkdir -p $OUTDIR
+STORAGE_DIR=/proj/uppmax2025-3-3/nobackup/tobia/04_2_checkm/storage
+OUTFILE=/proj/uppmax2025-3-3/nobackup/tobia/04_2_checkm/qa_results.tsv
 
-# Run CheckM lineage_wf (automated quality assessment)
-checkm lineage_wf -x fa -t 4 --reduced_tree $BINS_DIR $OUTDIR
+# Step 1: Analyze bins (generate marker statistics)
+checkm analyze -x fa --reduced_tree $STORAGE_DIR $BINS_DIR
+
+# Step 2: Create quality summary table
+checkm qa --tab_table -o 2 $STORAGE_DIR > $OUTFILE
