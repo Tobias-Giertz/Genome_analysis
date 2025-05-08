6ea5d66389da14bd4c41cbda7b548c95b3d68a84
Tobias-Giertz <tobias.giertz@gmail.com>
2025-05-08 18:55:47 +0200

    Create 042_bin_quality_checkm.sh

---
+++ ga_code/ga_code/042_bin_quality_checkm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2025-3-3
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH --mem=32G
+#SBATCH -t 04:00:00
+#SBATCH -J checkm_bins
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tobias.giertz.0318@student.uu.se
+#SBATCH --output=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/checkm-%j.out
+#SBATCH --error=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/checkm-%j.err
+
+# Load modules
+module load bioinfo-tools
+module load CheckM/1.1.3
+
+# Define paths
+BINS_DIR=/proj/uppmax2025-3-3/nobackup/tobia/03_binning
+OUTDIR=/proj/uppmax2025-3-3/nobackup/tobia/05_checkm
+mkdir -p $OUTDIR
+
+# Run CheckM lineage_wf (automated quality assessment)
+checkm lineage_wf -x fa -t 4 $BINS_DIR $OUTDIR
