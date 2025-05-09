994281e47571e4e27c2be87540231ac73a614db5
Tobias-Giertz <tobias.giertz@gmail.com>
2025-05-09 16:53:50 +0200

    Update 05_1_select_top_bins.sh

--- ga_code/05_1_select_top_bins.sh
+++ ga_code/05_1_select_top_bins.sh
@@ -1,19 +1,25 @@
-#!/bin/bash -l
+#!/bin/bash
 
-#SBATCH -A uppmax2025-3-3
-#SBATCH -M snowy
-#SBATCH -p core
-#SBATCH -n 1
-#SBATCH -t 00:10:00
-#SBATCH -J select_top_bins
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=tobias.giertz.0318@student.uu.se
-#SBATCH --output=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/select_top_bins-%j.out
-#SBATCH --error=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/select_top_bins-%j.err
+# Input CheckM summary file
+SUMMARY=/home/tobia/Genome_analysis/ga_analyses/04_quality_control/checkm_bin_summary.tsv
+OUTFILE=/home/tobia/Genome_analysis/ga_analyses/05_selected_bins/top_bins.tsv
 
-# Define paths
-INPUT_TSV=/home/tobia/Genome_analysis/ga_analyses/04_quality_control/checkm_bin_summary.tsv
-OUTPUT_TSV=/home/tobia/Genome_analysis/ga_analyses/04_quality_control/top_bins.tsv
+mkdir -p $(dirname "$OUTFILE")
 
-# Extract header and filter bins with completeness ≥ 70 and contamination ≤ 10
-awk -F '\t' 'NR==1 || ($6+0) >= 70 && ($7+0) <= 10' "$INPUT_TSV" > "$OUTPUT_TSV"
+# Filter bins based on quality criteria
+awk -F '\t' 'NR==1 || ($6 >= 70 && $7 <= 10)' "$SUMMARY" > "$OUTFILE"
+
+echo "Selected top bins saved to $OUTFILE"
+
+TOPBINS=/home/tobia/Genome_analysis/ga_analyses/05_selected_bins/top_bins.tsv
+ALLBINS=/proj/uppmax2025-3-3/nobackup/tobia/03_binning
+OUTDIR=/home/tobia/Genome_analysis/ga_analyses/05_selected_bins/top_bins_fa
+
+mkdir -p "$OUTDIR"
+
+tail -n +2 "$TOPBINS" | cut -f1 | while read bin_id; do
+    src="$ALLBINS/${bin_id/bin_/bin.}.fa"
+    dst="$OUTDIR/${bin_id}.fa"
+    cp "$src" "$dst" && echo "Copied $src → $dst"
+done
+
