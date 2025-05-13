5a5bf843e05cfc84b55a81af7bbf29576cc97707
Tobias-Giertz <tobias.giertz@gmail.com>
2025-05-13 19:35:45 +0200

    Update 05_2_taxonomy_gtdbtk.sh

--- ga_code/05_2_taxonomy_gtdbtk.sh
+++ ga_code/05_2_taxonomy_gtdbtk.sh
@@ -12,16 +12,20 @@
 #SBATCH --output=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/gtdbtk-%j.out
 #SBATCH --error=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/gtdbtk-%j.err
 
-# Load conda
+# Load GTDB-Tk from UPPMAX modules
 module load bioinfo-tools
-module load conda
-conda activate /proj/uppmax2025-3-3/nobackup/tobia/envs/gtdbtk_env
+module load GTDB-Tk/2.4.0
 
-# Define input/output
+# Set GTDB reference database
+export GTDBTK_DATA_PATH=/proj/uppmax2025-3-3/nobackup/tobia/gtdbtk_data/release207
+
+# Add HMMER binaries to path
+export PATH=/sw/bioinfo/hmmer/3.4/src/hmmer-3.4/src:$PATH
+
+# Define input and output
 INPUT_DIR=/home/tobia/Genome_analysis/ga_analyses/05_selected_bins/top_bins_fa
 OUTDIR=/proj/uppmax2025-3-3/nobackup/tobia/05_gtdbtk
+mkdir -p $OUTDIR
 
 # Run GTDB-Tk
 gtdbtk classify_wf --genome_dir $INPUT_DIR --out_dir $OUTDIR --cpus 16
-
-
