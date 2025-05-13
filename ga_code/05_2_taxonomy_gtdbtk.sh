c012c3645fa87ff954cdee853c063284a289d271
Tobias-Giertz <tobias.giertz@gmail.com>
2025-05-13 19:14:26 +0200

    Update 05_2_taxonomy_gtdbtk.sh

--- ga_code/05_2_taxonomy_gtdbtk.sh
+++ ga_code/05_2_taxonomy_gtdbtk.sh
@@ -13,13 +13,13 @@
 #SBATCH --error=/home/tobia/Genome_analysis/ga_code/ga_slurm_logs/gtdbtk-%j.err
 
 # Load conda
+module load bioinfo-tools
 module load conda
 conda activate /proj/uppmax2025-3-3/nobackup/tobia/envs/gtdbtk_env
 
 # Define input/output
 INPUT_DIR=/home/tobia/Genome_analysis/ga_analyses/05_selected_bins/top_bins_fa
 OUTDIR=/proj/uppmax2025-3-3/nobackup/tobia/05_gtdbtk
-mkdir -p $OUTDIR
 
 # Run GTDB-Tk
 gtdbtk classify_wf --genome_dir $INPUT_DIR --out_dir $OUTDIR --cpus 16
