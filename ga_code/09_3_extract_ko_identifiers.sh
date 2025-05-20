e004c574c2c6d08b72308b8ccfe8b7a27dec5814
Tobias-Giertz <tobias.giertz@gmail.com>
2025-05-20 20:42:37 +0200

    Create 09_3_extract_ko_identifiers.sh

---
+++ ga_code/09_3_extract_ko_identifiers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ANNOT_DIR=/domus/h1/tobia/Genome_analysis/ga_analyses/07_eggnog
+OUTDIR=${ANNOT_DIR}/summaries
+mkdir -p $OUTDIR
+
+for ANN in $ANNOT_DIR/bin_*_eggnog/*_emapper.annotations; do
+    BIN=$(basename "$ANN" _emapper.annotations)
+    OUT=$OUTDIR/${BIN}_ko.txt
+
+    grep -v "^#" "$ANN" | cut -f6 | tr ',' '\n' | grep "^K" | sort -u > "$OUT"
+    echo "Extracted KO list: $OUT"
+done
