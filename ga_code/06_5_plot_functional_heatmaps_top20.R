22b54e6d8f9faed2677b309440665b6675cca0b5
Tobias-Giertz <tobias.giertz@gmail.com>
2025-05-23 09:34:40 +0200

    Update 06_5_plot_functional_heatmaps_top20.R

--- ga_code/06_5_plot_functional_heatmaps_top20.R
+++ ga_code/06_5_plot_functional_heatmaps_top20.R
@@ -3,28 +3,22 @@ library(pheatmap)
 
 setwd("/domus/h1/tobia/Genome_analysis/ga_analyses/07_eggnog/summaries")
 
-# Function: top N by total count
-filter_top_n <- function(df, id_col, count_col, n = 20) {
-  top_ids <- df %>%
-    group_by({{id_col}}) %>%
-    summarise(total = sum({{count_col}})) %>%
-    arrange(desc(total)) %>%
-    slice_head(n = n) %>%
-    pull({{id_col}})
-  
-  df %>% filter({{id_col}} %in% top_ids)
-}
-
-# Function: plot heatmap
 plot_top_heatmap <- function(file, id_col, value_col, label, outfile, top_n = 20) {
   df <- read_tsv(file)
+  
+  # Filter out unknown category (e.g., "-")
+  df <- df %>% filter({{id_col}} != "-")
+  
+  # Select top N categories by total count
   df_top <- filter_top_n(df, {{id_col}}, {{value_col}}, top_n)
   
+  # Prepare matrix for heatmap
   mat <- df_top %>%
     pivot_wider(names_from = {{id_col}}, values_from = {{value_col}}, values_fill = 0) %>%
     column_to_rownames("Bin") %>%
     as.matrix()
   
+  # Generate heatmap
   pheatmap(mat, fontsize_row = 8, fontsize_col = 8,
            main = paste(label, "- Top", top_n),
            filename = outfile)
